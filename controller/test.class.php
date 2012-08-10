76c9e57f8510fd80e55a254668a8f5db37f82fdb
ishanigorule <ishanigorule@gmail.com>
2012-08-10 18:46:50 +0530

    inc

---
+++ controller/test.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of test
+ *
+ * @author ishanig
+ */
+class test {
+    //put your code here
+    function __construct() {
+    }
+
+    function setUser() {
+        echo 'tseted';
+    }
+}
+
+?>
--- core/Load.php
+++ core/Load.php
@@ -15,6 +15,7 @@ class Load {
     }
     
     function __call($func,$args) { 
+        echo $func;exit;
         if(method_exists($this, $func)) {
             call_user_func_array($func, $args);
         } else {
@@ -22,14 +23,23 @@ class Load {
         }
     }
     
-    function Controller($classname) {
-        $controller_file = 'controller/' . $classname . '.class.php';
-        if(file_exists($controller_file)) {
-            include_once($controller_file); 
-            return new $classname;
-        }
-        die('Invalid controller file');
-    }
+//    private function Controller($classname) {
+//        $controller_file = 'controller/' . $classname . '.class.php';
+//        if(file_exists($controller_file)) {
+//            include_once($controller_file); 
+//            return new $classname;
+//        }
+//        die('Invalid controller file');
+//    }
+//    
+//    private function View($viewname) {
+//        $view_file = 'views/' . $viewname . '.php';
+//        if(file_exists($view_file)) {
+//            include_once($view_file); 
+//        } else {
+//            die('Invalid view file');
+//        }
+//    }
 }
 
 ?>
